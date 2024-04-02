7b71e11c3b64b6e057004deb73d69222335777fb
Anthony Johnson <johns9au@mail.uc.edu>
2024-04-02 13:03:22 -0400

    Working battle System gonna edit it some more maybe work on start screen next

--- Foofen Fighters/Assets/BattleSystem.cs
+++ Foofen Fighters/Assets/BattleSystem.cs
@@ -19,13 +19,17 @@ public class BattleSystem : MonoBehaviour
     Unit enemyUnit;
 
     public Text dialogueText;
+
+    public BattleHUD playerHUD;
+    public BattleHUD enemyHUD;
+
     void Start()
     {
         state = BattleState.START;
-        setupBattle();
+        StartCoroutine(setupBattle());
     }
 
-    void setupBattle()
+    IEnumerator setupBattle()
     {
         GameObject playerGO = Instantiate(playerPrefab, playerBattleStation);
         playerUnit = playerGO.GetComponent<Unit>();
@@ -34,7 +38,106 @@ public class BattleSystem : MonoBehaviour
         enemyUnit = enemyGO.GetComponent<Unit>();
 
         dialogueText.text = "A wild " + enemyUnit.unitName + " approaches...";
+
+        playerHUD.SetHUD(playerUnit);
+        enemyHUD.SetHUD(enemyUnit);
+
+        yield return new WaitForSeconds(2f);
+
+        state = BattleState.PLAYERTURN;
+        PlayerTurn();
     }
 
+    IEnumerator PlayerAttack()
+    {
+        bool isDead = enemyUnit.TakeDamage(playerUnit.damage);
 
+        enemyHUD.SetHP(enemyUnit.currentHP);
+        dialogueText.text = "The attack is successful!";
+
+        yield return new WaitForSeconds(2f);
+
+        if(isDead)
+        {
+            state = BattleState.WON;
+            EndBattle();
+        }
+        else
+        {
+            state = BattleState.ENEMYTURN;
+            StartCoroutine(EnemyTurn());
+        }
+
+    }
+
+    IEnumerator
+        EnemyTurn()
+    {
+        dialogueText.text = enemyUnit.unitName + " attacks!";
+
+        yield return new WaitForSeconds(1f);
+
+        bool isDead = playerUnit.TakeDamage(enemyUnit.damage);
+
+        playerHUD.SetHP(playerUnit.currentHP);
+
+        yield return new WaitForSeconds(1f);
+
+        if(isDead)
+        {
+            state = BattleState.LOST;
+            EndBattle();
+        }
+        else
+        {
+            state = BattleState.PLAYERTURN;
+            PlayerTurn();
+        }
+    }   
+
+    IEnumerator PlayerHeal()
+    {
+        playerUnit.Heal(5);
+       
+        playerHUD.SetHP(playerUnit.currentHP);
+        dialogueText.text = "You feel renewed strength!";
+
+        yield return new WaitForSeconds(2f);
+
+        state = BattleState.ENEMYTURN;
+        StartCoroutine(EnemyTurn());
+    }
+
+    void EndBattle()
+    {
+        if(state == BattleState.WON)
+        {
+            dialogueText.text = "You won the battle!";
+        }
+        else if(state == BattleState.LOST)
+        {
+            dialogueText.text = "You were defeated.";
+        }
+    }
+    void PlayerTurn()
+    {
+        dialogueText.text = "Choose an action:";
+    }
+
+    public void onAttackButton()
+    {
+        if (state != BattleState.PLAYERTURN)
+            return;
+
+        StartCoroutine(PlayerAttack());
+    }
+    public void onHealButton()
+    {
+        if (state != BattleState.PLAYERTURN)
+            return;
+
+        StartCoroutine(PlayerHeal());
+    }
+
+    
 }
--- Foofen Fighters/Assets/Unit.cs
+++ Foofen Fighters/Assets/Unit.cs
@@ -13,5 +13,26 @@ public class Unit : MonoBehaviour
     public int maxHP;
     public int currentHP;
 
+    public bool TakeDamage(int dmg)
+    {
+        currentHP -= dmg;
+        
+        if(currentHP <= 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
 
+    public void Heal(int amount)
+    {
+        currentHP += amount;
+        if(currentHP > maxHP)
+        {
+            currentHP = maxHP;
+        }
+    }
 }
